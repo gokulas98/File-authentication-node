9e38db32f42ff792945bcefc4b9100e8a865647c
baizelkv <baizel.varghese@experionglobal.com>
2021-06-02 18:47:03 +0530

    added scripts to fetch username, password from database based on query parameter

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-// File : index.js
-
 const express = require("express");
-// const bodyParser = require("body-parser");
-
-
 const app = express();
+const pool = require("./db");
 
-// PORT
-const PORT = process.env.PORT || 4000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "API Working" });
+//ROUTES
+
+//Return user details based on a user_id
+
+app.get("/login/:emp_id/:password", async(req, res) => {
+    try {
+
+        const { emp_id, password } = req.params;
+        const details = await pool.query( "SELECT emp_id, password FROM registered_employee WHERE emp_id = $1 AND password = $2",[emp_id, password]);
+        if(details.rows.length == 0) {
+            res.status(400);
+            res.send("Error");
+        }
+        else {
+            res.status(200);
+            res.json(details.rows[0]);
+        }
+
+    }catch(err) {
+        console.error(err.message);
+    }
 });
 
-
-
-var authn=false;                  //dummy variable
-
-app.get("/authn", (req,res)=> {
-
-
-  console.log(req.body)
-  if(authn==true){
-    res.set('Content-Type', 'text/plain')
-    res.status(200).send('Successfully logged in')
-    console.log("User authenicated")
-  }
-  else{
-    res.status(401).send('Wrong credentials')
-    console.log("User not authenticated")
-
-  }
-})
-
-
-
-app.listen(PORT, (req, res) => {
-  console.log(`Server Started at PORT ${PORT}`);
+app.listen(3000, () => { 
+    console.log("server is listening on port 3000");
 });
