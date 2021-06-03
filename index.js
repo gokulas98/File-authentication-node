373192f835aa5d8e9f15c0e3d0956b519c68ecfb
gokulas98 <gokul.as@experionglobal.com>
2021-06-03 18:17:36 +0530

    Added function to authenticate logged in user with the hashed password stored in DB

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 const express = require("express");
 const app = express();
 const pool = require("./db");
-
+const bcrypt= require("bcrypt")
+ 
 app.use(express.json());
-
+ 
 //git
-
+ 
 //ROUTES
-
+ 
 //Return user details based on a user_id
-
+ 
 app.get("/login/:emp_id/:password", async(req, res) => {
     try {
-
-        const { emp_id, password } = req.params;
-        const details = await pool.query( "SELECT emp_id, password FROM registered_employee WHERE emp_id = $1 AND password = $2",[emp_id, password]);
+ 
+        const {emp_id,} = req.params;
+        //const details = await pool.query( "SELECT emp_id, password FROM registered_employee WHERE emp_id = $1 AND password = $2",[emp_id, password]);
         
-        //Authentication funcion******************
+        //Authentication check******************
+        const hash = await pool.query( "SELECT password_digest FROM registered_employee WHERE emp_id = $1 ",[emp_id]);
+        console.log(hash.rows[0].password_digest)
 
-
-        if(authn==true){
+        bcrypt.compare(req.params.password, hash.rows[0].password_digest, function(error, result) {
+            // if res == true, password matched
+            // else wrong password
+            if (result==true){
+            console.log("Password matched")
             res.set('Content-Type', 'text/plain')
             res.status(200).send('Successfully logged in')
             console.log("User authenicated")
-          }
-          else{
+            }else{
+            console.log("Wrong password")
             res.status(401).send('Wrong credentials')
             console.log("User not authenticated")
-        
-          }
-
+            }
+        });
+ 
     }catch(err) {
         console.error(err.message);
     }
 });
+ 
+
 
 app.listen(3000, () => { 
     console.log("server is listening on port 3000");
